b12fab1c536f9ccf7e1f710d6e00e652ff4d2b6a
Artur Marques Magalhães <34356169+arturmmagalhaes@users.noreply.github.com>
2020-08-06 17:31:47 -0300

    Delete Expertise.ts

--- src/Expertise.ts
+++
@@ -1,9 +0,0 @@
-export enum Expertise {
-    React       = "React",
-    Redux       = "Redux",
-    CSS         = "CSS",
-    Testes      = "Testes",
-    Typescript  = "Typescript",
-    POO         = "Programação Orientada à Objetos",
-    Backend     = "Backend"
-}
